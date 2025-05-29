5787cd3cab1bb6bca0df9a024d21ec959f25e6b2
liana2909 <lmkrtchian1@gmail.com>
2025-05-29 13:41:56 +0300

    Update generate.py
    Updated generate.py to support input image

--- lumina_mgpt/generate_examples/generate.py
+++ lumina_mgpt/generate_examples/generate.py
@@ -27,13 +27,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     print("args:\n", args)
-    l_prompts = [
-        "Image of a dog playing water, and a water fall is in the background.",
-        "A high-resolution photograph of a middle-aged woman with curly hair, wearing traditional Japanese kimono, smiling gently under a cherry blossom tree in full bloom.",  # noqa
-        "A pink and chocolate cake ln front of a white background",
-        "A highly detailed, 3D-rendered, stylized representation of 2 travellers, a 40 year old man and a step behind, a 40 year old woman walking on a path. Full body visible. They have large, expressive hazel eyes and dark, curly hair that is slightly messy. Their faces are full of innocent wonder, with rosy cheeks and a scattering of light freckles across his nose and cheeks. They are wearing thicker clothes, trousers and hiking shoes, making it look cosy. The background is softly blurred with warm tones, putting full focus on their facial features and expressions. The image has a soft, cinematic lighting style with subtle shadows that highlight the contours of their faces, giving it a realistic yet animated look. The overall art style is similar to modern animated films with high levels of detail and a slight painterly touch, 8k.",
-        "Indoor portrait of a young woman with light blonde hair sitting in front of a large window. She is positioned slightly to the right, wearing an oversized white shirt with rolled-up sleeves and brown pants. A black shoulder bag is slung over her left shoulder. Her lips are pursed in a playful expression. The window behind her features closed horizontal blinds, reflecting faint interior lighting. The surrounding wall is made of textured, light-colored brick. The lighting is soft, highlighting her features and the textures of her clothing and the wall. Casual, candid, slightly cool color temperature, natural pose, balanced composition, urban interior environment."
-    ]
 
     t = args.temperature
     top_k = args.top_k
@@ -71,26 +64,18 @@ if __name__ == "__main__":
             prefix_token_sampler_scheme='speculative_jacobi',
             is_compile=args.quant
         )
+        
+    input_img = Image.open("input_images/img_046.jpeg").convert("RGB")
+    prompt = "Make a black and white coloring book outline from the given photo"
 
     with torch.no_grad():
-        for i, prompt in enumerate(l_prompts):
-            for repeat_idx in range(n):
-                random_seed(repeat_idx)
-                if args.task == 't2i':
-                    generated = inference_solver.generate(
-                            images=[],
-                            qas=[[f"Generate an image of {w}x{h} according to the following prompt:\n{prompt}", None]],  # high-quality synthetic  superior
-                            max_gen_len=10240,
-                            temperature=t,
-                            logits_processor=inference_solver.create_logits_processor(cfg=cfg, image_top_k=top_k),
-                        )
-                else:
-                    task_dict = {"depth": "depth map", "canny": "canny edge map", "hed": "hed edge map", "openpose":"pose estimation map"}
-                    generated = inference_solver.generate(
-                            images=[],
-                            qas=[[f"Generate a dual-panel image of {w}x{h} where the <lower half> displays a <{task_dict[args.task]}>, while the <upper half> retains the original image for direct visual comparison:\n{prompt}" , None]], 
-                            max_gen_len=10240,
-                            temperature=t,
-                            logits_processor=inference_solver.create_logits_processor(cfg=cfg, image_top_k=top_k),
-                            )
-                generated[1][0].save(args.save_path + f"{i}.png")
+        for repeat_idx in range(n):
+            random_seed(repeat_idx)
+            generated = inference_solver.generate(
+                images=[input_img],
+                qas=[[prompt, None]],
+                max_gen_len=10240,
+                temperature=t,
+                logits_processor=inference_solver.create_logits_processor(cfg=cfg, image_top_k=top_k),
+            )
+            generated[1][0].save(os.path.join(args.save_path, f"result_{repeat_idx}.png"))        
